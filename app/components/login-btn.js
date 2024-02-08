419e337d3cad2a0a868668adfd390ff11e6c0e9d
Rabby Hasan <rabbyalone@gmail.com>
2024-02-08 17:11:18 +0600

    added loading ui

--- app/components/login-btn.js
+++ app/components/login-btn.js
@@ -6,7 +6,7 @@ export default function Component() {
     if (session) {
         return (
             <>
-                <span className="text-white">{session.user.email}</span>
+                <span className="text-white mr-2">{session.user.email}</span>
                 <button className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                     onClick={(e) => {
                         e.preventDefault();
--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,12 @@
 export default function Loading() {
-    return <p>Loading...</p>
-  }
-  
-  
+    return (
+        <div class="flex justify-center items-center h-screen">
+            <div class="relative inline-flex">
+                <div class="w-8 h-8 bg-blue-500 rounded-full"></div>
+                <div class="w-8 h-8 bg-blue-500 rounded-full absolute top-0 left-0 animate-ping"></div>
+                <div class="w-8 h-8 bg-blue-500 rounded-full absolute top-0 left-0 animate-pulse"></div>
+            </div>
+        </div>
+    )
+}
+
