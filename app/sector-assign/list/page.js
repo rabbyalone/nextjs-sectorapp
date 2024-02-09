25722fd8126f3c38e3f9225087566ea07978a9e6
Rabby Hasan <rabbyalone@gmail.com>
2024-02-09 08:15:25 +0600

    route fixed

--- app/sector-assign/list/page.js
+++ app/sector-assign/list/page.js
@@ -13,7 +13,7 @@ export default async function ListPage() {
         <div className="w-75 mx-auto glass border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-auto">
             <div className="p-5">
                 <h2 className="card-title mb-5">List of Assigned Sectors</h2>
-                <Link className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-5' href="/sector/create">Add New</Link>
+                <Link className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-5' href="/sector-assign/assign">Add New</Link>
 
                 <table className='table mt-5'>
                     <thead>
